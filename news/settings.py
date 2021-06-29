01227e5bfc1ffec1912ff96a384e2e8d7c23ca48
jlucasoliveira <lucasosilva68@gmail.com>
2021-06-29 16:10:56 -0300

    feat: adding rest on installed apps

--- news/settings.py
+++ news/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "users.apps.UsersConfig",
 ]
 
 MIDDLEWARE = [
@@ -99,6 +102,23 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+
+AUTH_USER_MODEL = "users.User"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+    ],
+    "EXCEPTION_HANDLER": "exceptions_hog.exception_handler",
+}
 
 
 # Internationalization
