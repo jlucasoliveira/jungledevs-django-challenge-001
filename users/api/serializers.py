a01ac5de991b0ebfa3a9f2aac000a9acb3e7cd72
jlucasoliveira <lucasosilva68@gmail.com>
2021-06-29 16:13:26 -0300

    feat: authentication validators on serializers

---
+++ users/api/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+
+User = get_user_model()
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+
+class RegisterSerializer(serializers.Serializer):
+    email = serializers.EmailField(
+        validators=[
+            UniqueValidator(queryset=User.objects.all()),
+        ]
+    )
+    name = serializers.CharField(max_length=200)
+    password = serializers.CharField(read_only=True, source="password1")
+    password1 = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        if attrs["password1"] != attrs["password2"]:
+            raise serializers.ValidationError(_("As senhas não coincidem."))
+        return super().validate(attrs)
+
+
+class AuthenticatedUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "auth_token",
+            "name",
+            "email",
+        )
